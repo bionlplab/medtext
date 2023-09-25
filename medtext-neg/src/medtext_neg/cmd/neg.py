f47567ac44be50247df6916f0adabef198118dff
SONG <wangsong_1996@hotmail.com>
2023-09-24 21:37:23 -0500

    deep learning based negation classification

--- medtext-neg/src/medtext_neg/cmd/neg.py
+++ medtext-neg/src/medtext_neg/cmd/neg.py
@@ -12,58 +12,26 @@ Options:
     --sort-anns             Sort annotations by its location
 """
 import os.path
-
 import bioc
-
-from medtext_commons.download_utils import request_medtext, MEDTEXT_RESOURCES_GITHUB
 from medtext_commons.core import BioCPipeline
-
 
 import docopt
 from medtext_commons.cmd_utils import process_options, process_file
-from medtext_neg.models.match_ngrex import NegGrexPatterns
-from medtext_neg.models.neg import NegRegexPatterns
 from medtext_neg.models.neg_cleanup import NegCleanUp
 from medtext_neg.models.neg import BioCNeg
-
-
-# DEFAULT_RADLEX = Path.home() / '.medtext/resources/Radlex4.1.xlsx'
-'''
-    --regex-negation FILE               [default: ~/.medtext/resources/patterns/regex_negation.yml]
-    --regex-uncertainty-pre-neg FILE    [default: ~/.medtext/resources/patterns/regex_uncertainty_pre_negation.yml]
-    --regex-uncertainty-post-neg FILE   [default: ~/.medtext/resources/patterns/regex_uncertainty_post_negation.yml]
-    --regex-double-neg FILE             [default: ~/.medtext/resources/patterns/regex_double_negation.yml]
-    --ngrex-negation FILE               [default: ~/.medtext/resources/patterns/ngrex_negation.yml]
-    --ngrex-uncertainty-pre-neg FILE    [default: ~/.medtext/resources/patterns/ngrex_uncertainty_pre_negation.yml]
-    --ngrex-uncertainty-post-neg FILE   [default: ~/.medtext/resources/patterns/ngrex_uncertainty_post_negation.yml]
-    --ngrex-double-neg FILE             [default: ~/.medtext/resources/patterns/ngrex_double_negation.yml]
-'''
-
 
 def main():
     argv = docopt.docopt(__doc__)
     process_options(argv)
 
     if argv['neg']:
-        regex_actor = NegRegexPatterns()
-        regex_actor.load_yml2(os.path.expanduser(argv['--regex-patterns']))
-        ngrex_actor = NegGrexPatterns()
-        ngrex_actor.load_yml2(os.path.expanduser(argv['--ngrex-patterns']))
-
-        neg_actor = BioCNeg(regex_actor=regex_actor, ngrex_actor=ngrex_actor)
-        cleanup_actor = NegCleanUp(argv['--sort_anns'])
+        neg_actor = BioCNeg()
+        cleanup_actor = NegCleanUp()
         pipeline = BioCPipeline()
         pipeline.processors = [neg_actor, cleanup_actor]
-
         process_file(argv['-i'], argv['-o'], pipeline, bioc.PASSAGE)
-    elif argv['download']:
-        request_medtext(os.path.expanduser(argv['--regex-patterns']),
-                        src=MEDTEXT_RESOURCES_GITHUB + '/patterns/regex_patterns.yml')
-        request_medtext(os.path.expanduser(argv['--ngrex-patterns']),
-                        src=MEDTEXT_RESOURCES_GITHUB + '/patterns/ngrex_patterns.yml')
     else:
         raise KeyError
-
 
 if __name__ == '__main__':
     main()
